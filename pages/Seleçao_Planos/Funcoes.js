1f12f638077850edcfaeaadd9fb42460f92ba5d7
unknown <andrejoas93@gmail.com>
2023-02-18 22:40:35 -0300

    comitadoByLocal

--- pages/Seleçao_Planos/Funcoes.js
+++ pages/Seleçao_Planos/Funcoes.js
@@ -7,44 +7,44 @@ function Mycolor() {
     var element3 = document.getElementById("botaoLigth");
 
     var elem = document.getElementById("botaoNight");
-    elem.style.border="1px solid #000000";
-    elem.style.color="#fff";
+    elem.style.border = "1px solid #000000";
+    elem.style.color = "#fff";
     element.style.backgroundColor = '#92a4ec';
     element.style.color = '#fff';
-    element3.style.backgroundColor='#fff';
-    element3.style.color='#000000';
+    element3.style.backgroundColor = '#fff';
+    element3.style.color = '#000000';
 }
 function Mycolor2() {
     var element = document.getElementById("myID");
     var elementobt = document.getElementById("botaoNight");
     element.style.backgroundColor = '#fff',
-    element.style.color = '#000000';
-    elementobt.style.backgroundColor="#000000";
-    elementobt.style.color="#fff";
+        element.style.color = '#000000';
+    elementobt.style.backgroundColor = "#000000";
+    elementobt.style.color = "#fff";
 }
 
-function bordar(){
+function bordar() {
     var card1 = document.getElementById("card");
-    card1.style.border="solid 2px #000000";
-    
+    card1.style.border = "solid 2px #000000";
+
 }
-function bordar2(){
+function bordar2() {
     var card2 = document.getElementById("card2");
-    card2.style.border="solid 2px #000000";
-    
+    card2.style.border = "solid 2px #000000";
+
 }
-function bordar3(){
+function bordar3() {
     var card3 = document.getElementById("card3");
-    card3.style.border="solid 2px #000000";
-    
+    card3.style.border = "solid 2px #000000";
+
 }
-function Desmarcar(){
+function Desmarcar() {
     var desmaca = document.getElementById("card");
     var desmaca1 = document.getElementById("card2");
     var desmaca2 = document.getElementById("card3");
-    desmaca.style.border="none";
-    desmaca1.style.border="none";
-    desmaca2.style.border="none";
+    desmaca.style.border = "none";
+    desmaca1.style.border = "none";
+    desmaca2.style.border = "none";
 }
 
 
@@ -60,22 +60,42 @@ var lista1 = [0]
 
 function Clicar() {
     lista1.push(lista2[0])
-    resultado += parseInt(lista1[0]+mes1);
+    resultado += parseInt(lista1[0] + mes1);
     // alert(lista1)
     // alert(resultado)
-    document.getElementById("resultado").innerHTML=resultado;
+    document.getElementById("resultado").innerHTML = resultado;
 }
 function Clicar2() {
     lista1.push(lista2[1])
-    resultado += parseInt(lista1[0]+mes2);
+    resultado += parseInt(lista1[0] + mes2);
     // alert(lista1)
     // alert(resultado)
-    document.getElementById("resultado").innerHTML=resultado;
+    document.getElementById("resultado").innerHTML = resultado;
 }
 function Clicar3() {
     lista1.push(lista2[2])
-    resultado += parseInt(lista1[0]+mes3);
+    resultado += parseInt(lista1[0] + mes3);
     // alert(lista1)
-    document.getElementById("resultado").innerHTML=resultado;
+    document.getElementById("resultado").innerHTML = resultado;
 
+}
+
+
+
+// ------------- step 04
+var a = document.getElementById('ret1');
+
+
+
+var a = 0;
+function moveDiv() {
+    let divEmPrograsso = document.getElementById("ret1").innerHTMLd;
+    // divEmPrograsso.appendChild(div.parentElement);
+    document.getElementById("planos").appendChild(divEmPrograsso)
+    console.log("foi")
+}
+
+
+function sele() { 
+moveDiv();
 }
